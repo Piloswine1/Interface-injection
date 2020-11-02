802c30df147f91796ae00544110fb408705db6ff
Gleb Lysov <lysovg9@gmail.com>
2020-11-02 22:04:49 +0300

    fixes

--- essentials.ts
+++ essentials.ts
@@ -1,5 +1,5 @@
 // import * as log from "https://deno.land/std@0.74.0/log/mod.ts"
-import { readLines, StringWriter, StringReader, BufReader } from "https://deno.land/std@0.74.0/io/mod.ts";
+import { readLines, StringWriter, StringReader } from "https://deno.land/std@0.74.0/io/mod.ts";
 import { walkSync } from "https://deno.land/std@0.74.0/fs/mod.ts"; //XXX: may be unstable
 import { range } from "https://unpkg.com/@newdash/newdash-deno/range.ts";
 import { equal } from "https://deno.land/std@0.75.0/bytes/mod.ts";
@@ -37,6 +37,7 @@ let NEWLINE: "\n" | "\r\n" = "\r\n";
 let _TAB = "";
 
 const BOM = encoder.encode("\u{feff}");
+const BOM_STR = "\u{feff}";
 const S_KEY = 115
 const TAB = (n: number) => {
   let ret = _TAB;
@@ -47,10 +48,12 @@ const CLASS = "class "
 const ABSTRACT = "abstract "
 const USING ="using System;" //utf 8 - BOM
 const INTERFACE = "IDisposable";
+const DISPOSE_OLD = "public void Dispose() {throw new NotImplementedException();}";
+const DISPOSE = "public void Dispose()";
 const INTERFACE_IMPL = (n: number = 0) => {
   const TAB1 = TAB(n)
   const TAB2 = TAB1 + "\t"
-  return TAB1 + "public void Dispose()" +
+  return TAB1 + DISPOSE +
     NEWLINE + TAB1 + "{" +
     NEWLINE + TAB2 + "throw new NotImplementedException();" +
     NEWLINE + TAB1 + "}";
@@ -115,18 +118,157 @@ const OpenFiles: OpenFilesType = async (filename)  => {
   return [files, cleanUp]
 }
 
+type CB = (s: string)=>void
+
+//UTILS___________________________________________
 const copyFileTo = async (from: OpenFilesRet, to: Deno.Writer) => {
   await Deno.seek(from.rid, 0, Deno.SeekMode.Start);
   await Deno.copy(from.self, to);  
 }
 
+const format_class = (str: string, cb: CB) => {
+  str = str.trimEnd();
+  cb(str);
+  return ({
+    isDotted: str.includes(":"),
+    isComma: str[str.length - 1] === ","
+  });
+};
+
+const cut_custom = (str: string, delim: string, cb: CB) => {
+  const pos = str.indexOf(delim);
+  let tail = ""
+  if (pos !== -1) {
+    tail = str.substr(pos);
+    str = str.substr(0, pos);
+  }
+  cb(str)
+  return tail;
+};
+
+const count_parent = (str: string) => {
+  const opened = str.match("{");
+  const closed = str.match("}");
+  const num = (opened?.length ?? 0) - (closed?.length ?? 0)
+  return {
+    opened: opened?.length,
+    closed: closed?.length,
+    num
+  }
+}
+//________________________________________________
+
 /**
- * @deprecated
  * @param filename 
  * @param show 
  */
+const FixFile_OLD: AsyncFormatFile = async (filename, show) => {
+  const [[file, fileNew], cleanUp] = await OpenFiles(filename);
+
+  let _interface = false,
+    _dispose = false,
+    _using = false;
+
+  for await (const line of readLines(file.self)) {
+    if (line.includes(USING)) _using = true;
+    if (line.includes(INTERFACE)) _interface = true;
+    if (line.includes(DISPOSE)) _dispose = true;
+  }
+
+  if (_interface && _dispose) {
+    if (_using) {
+      console.log("not fixing");
+    } else {
+      console.error("no using");
+    }
+    cleanUp(true);
+    return
+  }
+
+  await Deno.seek(file.rid, 0, Deno.SeekMode.Start);
+
+  for await (const line of readLines(file.self)) {  
+    let to_print = line.trimEnd();  
+    let cb = (str: string) => {to_print = str};
+
+    const comments = cut_custom(to_print, "//", cb);
+
+    if (to_print.includes(CLASS)      && 
+        !to_print.includes(INTERFACE) &&
+        !to_print.includes(ABSTRACT)) {
+      const paren = cut_custom(to_print, "{", cb);
+      const {isComma, isDotted} = format_class(to_print, cb);
+      if (!isDotted) {
+        to_print += ":";
+      } else if (!isComma) {
+        to_print += ",";
+      }
+      to_print += " " + INTERFACE;
+      if (isComma) to_print += ","
+      to_print += paren;
+    }
+
+    to_print = to_print + comments + NEWLINE;
+
+    await fileNew.write(to_print);
+  }
+
+  if (show) await copyFileTo(fileNew, Deno.stdout);
+  cleanUp(false);
+  console.log("fixed");
+}
+
+const FixFile_BOM: AsyncFormatFile = async (filename, show) => {
+  const [[file, fileNew], cleanUp] = await OpenFiles(filename);
+
+  let changing = false;
+  let first = true;
+
+  for await (const _line of readLines(file.self)) {
+    let line = _line.trimEnd();
+    if (first) {
+      if (!line.includes(BOM_STR)) {
+        line = BOM_STR + line;
+      }
+      first = false;
+    } else {
+      if (line.includes(BOM_STR)) {
+        changing = true;
+        line = line.substr(line.indexOf(BOM_STR) + BOM_STR.length);
+      } 
+    }
+    if (line.includes(DISPOSE_OLD)) {
+      line = INTERFACE_IMPL(2); 
+      changing = true;
+    }
+    await fileNew.write(line + NEWLINE);
+  }
+
+  if (show) await copyFileTo(fileNew, Deno.stdout);
+  if (changing) {
+    console.log("fixed")
+  } else {
+    console.log("not fixing");
+  }
+  cleanUp(!changing);
+}
+
 const FixFile: AsyncFormatFile = async (filename, show) => {
-  throw Error("deprecated");
+  const [[file, fileNew], cleanUp] = await OpenFiles(filename);
+  let paren: number | null = null;
+
+  for await (const _line of readLines(file.self)) {
+    const line =  _line.trimEnd()
+    const {num, opened} = count_parent(line);
+    if (opened && paren === null) paren = 0;
+    if (paren !== null) paren += num;
+    await fileNew.write(line + NEWLINE);
+    if (paren === 0) break;
+  }
+
+  if (show) await copyFileTo(fileNew, Deno.stdout);
+    console.log("fixed")
+  cleanUp(false);
 }
 
 /**
@@ -181,47 +323,20 @@ const FormatFile: AsyncFormatFile = async (filename, show) => {
     //in local classes it will fuck up... or not
     let to_print = line.trimEnd();
     //cut comments from to_print and return  it
-    const cut_custom = (delim: string) => {
-      const pos = to_print.indexOf(delim);
-      if (pos !== -1) {
-        const tail = to_print.substr(pos);
-        to_print = to_print.substr(0, pos);
-        return tail;
-      }
-      return "";
-    };
-    const format_class = () => {
-      to_print = to_print.trimEnd();
-      // console.log(to_print);
-      return ({
-        isDotted: to_print.includes(":"),
-        isComma: to_print[to_print.length - 1] === ","
-      })
-    }
-    const count_parent = () => {
-      const opened = to_print.match("{");
-      const closed = to_print.match("}");
-      const num = (opened?.length ?? 0) - (closed?.length ?? 0)
-      return {
-        opened: opened?.length,
-        closed: closed?.length,
-        num
-      }
-    }
+    let cb = (str: string) => {to_print = str};
 
-    const {num, opened, closed} = count_parent();
+    const {num, opened, closed} = count_parent(to_print);
 
     if (opened && opened !== 0) range(opened).forEach(_ => _TAB += "\t");
     if (closed && closed !== 0) _TAB = _TAB.substr(0, _TAB.length - closed);
     // console.log({_TAB, num, opened, closed, openedParent});
 
-    const comments = cut_custom("//");
+    const comments = cut_custom(to_print, "//", cb);
     if (to_print.includes(CLASS)      && 
         !to_print.includes(INTERFACE) &&
         !to_print.includes(ABSTRACT)) {
-      
-      const paren = cut_custom("{");
-      const {isComma, isDotted} = format_class();
+      const paren = cut_custom(to_print, "{", cb);
+      const {isComma, isDotted} = format_class(to_print, cb);
       if (!isDotted) {
         to_print += ":";
       } else if (!isComma) {
@@ -236,7 +351,7 @@ const FormatFile: AsyncFormatFile = async (filename, show) => {
     if (openedParent !== null) {
       openedParent += num
       if (closed && openedParent === 0) {
-        const paren = cut_custom("}");
+        const paren = cut_custom(to_print, "}", cb);
         to_print = to_print.trimEnd();
         to_print += NEWLINE + INTERFACE_IMPL((paren)?1:0) + 
                     NEWLINE + _TAB + paren;
